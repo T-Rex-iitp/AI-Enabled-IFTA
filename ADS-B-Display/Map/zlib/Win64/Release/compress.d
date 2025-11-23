6cae01aa54474b5675cb55683617ad96370e612d
seunghahh <seunghahh@naver.com>
2025-11-22 19:26:24 -0800

    adsb_1122

---
+++ ADS-B-Display/Map/zlib/Win64/Release/compress.d
@@ -0,0 +1,3 @@
+.\Win64\Release\compress.o: compress.c \
+  C:\Users\admin\Desktop\t-rex\ADS-B-Display\Map\zlib/zlib.h \
+  C:\Users\admin\Desktop\t-rex\ADS-B-Display\Map\zlib/zconf.h
---
+++ ADS-B-Display/Map/zlib/Win64/Release/uncompr.d
@@ -0,0 +1,3 @@
+.\Win64\Release\uncompr.o: uncompr.c \
+  C:\Users\admin\Desktop\t-rex\ADS-B-Display\Map\zlib/zlib.h \
+  C:\Users\admin\Desktop\t-rex\ADS-B-Display\Map\zlib/zconf.h
